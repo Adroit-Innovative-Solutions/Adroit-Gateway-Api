104cd2806949b4f1d514ee77cd28b12539ce2914
arunreddy17 <putluruarunkumarreddy18@gmail.com>
2025-07-18 11:15:23 +0530

    Created APIGateway

--- src/main/java/io/datquad/ApiGateway/ApiGatewayApplication.java
+++ src/main/java/io/datquad/ApiGateway/ApiGatewayApplication.java
@@ -3,7 +3,6 @@ package io.datquad.ApiGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
